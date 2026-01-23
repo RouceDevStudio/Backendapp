f892a581873b02813c5e691b905fcef60fe7a6e2
Jhonatan Castro | Desarrollador Web <168943236+RouceDevStudio@users.noreply.github.com>
2026-01-23 16:39:20 -0500

    Se agregan funciones de backend
    Con estas funciones se pueden almacenar y mostrar la información en tiempo real

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+// "base de datos" temporal
+let data = []
+
+// RECIBIR info (front input)
+app.post("/games", (req, res) => {
+  const item = req.body
+  data.push(item)
+
+  res.json({
+    ok: true,
+    message: "Dato recibido y guardado"
+  })
+})
+
+// ENVIAR info (front output)
+app.get("/games", (req, res) => {
+  res.json(data)
+})
+
+app.listen(PORT, () => {
+  console.log("Backend corriendo en puerto", PORT)
+})
