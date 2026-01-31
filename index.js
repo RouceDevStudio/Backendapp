1f083b2f0166447c0f1529e4a9701ec5de88ca33
Roucedev <168943236+RouceDevStudio@users.noreply.github.com>
2026-01-31 14:00:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -39,10 +39,12 @@ app.use(helmet({
 }));
 
 const allowedOrigins = [
+    'https://roucedevstudio.github',
     'https://roucedevstudio.github.io',
     'http://localhost:3000',
     'http://localhost:5500',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'http://localhost:7700'
 ];
 
 app.use(cors({
